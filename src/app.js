200f3e9f8682bc526f850e34b4d018000fa7000c
EXI-Ramalingam <ranjith.ramalingam@boeing.com>
2018-11-29 15:31:00 -0800

    27: React Component, Nesting component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,60 @@
-class Header {}
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOptions />
+      </div>
+    );
+  }
+}
+class Header extends React.Component {
+  render() {
+    return <p>This is from Header</p>;
+  }
+}
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should i do</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        Option Components go hear
+        <Option />
+      </div>
+    );
+  }
+}
+class Option extends React.Component {
+  render() {
+    return (
+      <ol>
+        <li>Read</li>
+        <li>Write</li>
+      </ol>
+    );
+  }
+}
+class AddOptions extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
