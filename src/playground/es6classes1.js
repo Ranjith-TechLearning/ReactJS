597cb5c2cdcb08c5640bdf2ae27d90d3f1556cc1
EXI-Ramalingam <ranjith.ramalingam@boeing.com>
2018-11-29 14:59:47 -0800

    25: es6 classes and subclasses

--- src/playground/es6classes1.js
+++ src/playground/es6classes1.js
@@ -7,10 +7,46 @@ class Person {
     console.log();
     return `${this.name} is ${this.age} year(s) old`;
   }
+  getGreeting() {
+    return `Hi. I am ${this.name} `;
+  }
 }
 
-const person1 = new Person("Ranjith Ramalingam", 35);
+class Student extends Person {
+  constructor(name, age, major = "Science") {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+    return description + ` and their major is ${this.major} `;
+  }
+}
+
+const person1 = new Student("Ranjith Ramalingam", 35, "computer science");
 console.log(person1.getDescription());
 
-const person2 = new Person();
+const person2 = new Student();
 console.log(person2.getDescription());
+
+class Traveler extends Person {
+  constructor(name, age, homelocation) {
+    super(name, age);
+    this.homelocation = homelocation;
+  }
+  getGreeting() {
+    const greetingFromSuper = super.getGreeting();
+    return this.homelocation
+      ? greetingFromSuper + ` I am visiting from ${this.homelocation}`
+      : greetingFromSuper;
+  }
+}
+
+const traveller1 = new Traveler("Ranjith", 28, "Seattle");
+console.log(traveller1.getGreeting());
+
+const traveller2 = new Traveler("Ranjith", 28);
+console.log(traveller2.getGreeting());
